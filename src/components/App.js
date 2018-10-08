2e221d4b4e8b368a988e65681bf43e7ef8132ead
jamal7X7 <jamal7x7@gmail.com>
2018-10-08 19:28:46 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+const Header = () => (
+  <header className='App-header'>
+    <h1 className='App-title'> Arcade Shooter App </h1>
+  </header>
+)
+
+const Main = (props) => (
+  <div className='game'>
+
+    
+
+  </div>
+)
+
+const Side = () => (
+  <div className='score'>
+
+      
+
+  </div>
+)
+
+
+
+class  ArcadeShooterApp extends Component {
+  
+  state = {
+    
+  }
+
+
+
+  componentDidMount() {
+    
+  
+
+  }
+
+
+      render () {
+    return (
+      <div className='App-container'>
+
+        <Side />
+        <Main />
+       
+
+      </div>
+    )
+  }
+}
+
+const App = (props) => (
+  <div className='App'>
+    <Header />
+    < ArcadeShooterApp />
+
+  </div>
+)
+
+export default App
+
